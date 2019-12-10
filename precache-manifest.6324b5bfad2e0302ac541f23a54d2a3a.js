4701fac09863fdcb051b5fdb8a93847505791e40
flameddd <flameddd@users.noreply.github.com>
2019-12-10 02:18:57 +0000

    Automated deployment: Tue Dec 10 02:18:57 UTC 2019 323ee5fd727457984c403fa36c43688a29df357d

---
+++ precache-manifest.6324b5bfad2e0302ac541f23a54d2a3a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/OtterPlayer/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/OtterPlayer/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/OtterPlayer/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/OtterPlayer/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/OtterPlayer/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/OtterPlayer/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "c395157dc860738eef84bbf993a4168e",
+    "url": "/OtterPlayer/assets/icons/videojsFavicon.png"
+  },
+  {
+    "revision": "66be6940e205df3cc917",
+    "url": "/OtterPlayer/bundle.66be6.js"
+  },
+  {
+    "revision": "66be6940e205df3cc917",
+    "url": "/OtterPlayer/bundle.fd466.css"
+  },
+  {
+    "revision": "78a0c4e8e5ceff5afa7bb44ec1dc49be",
+    "url": "/OtterPlayer/index.html"
+  },
+  {
+    "revision": "f2916f1c0a8f1199c034",
+    "url": "/OtterPlayer/polyfills.f2916.js"
+  },
+  {
+    "revision": "57fec53d680749b4fe7f",
+    "url": "/OtterPlayer/route-about.chunk.39129.css"
+  },
+  {
+    "revision": "57fec53d680749b4fe7f",
+    "url": "/OtterPlayer/route-about.chunk.57fec.js"
+  },
+  {
+    "revision": "4b5d8f791a78ca07918c",
+    "url": "/OtterPlayer/route-home.chunk.4b5d8.js"
+  },
+  {
+    "revision": "4b5d8f791a78ca07918c",
+    "url": "/OtterPlayer/route-home.chunk.bda64.css"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/OtterPlayer/sw-debug.js"
+  },
+  {
+    "revision": "ad9170f6705a39914d0a5f4053677ed5",
+    "url": "/OtterPlayer/sw-esm.js"
+  },
+  {
+    "revision": "9a3cb81a74444b1e69ff617c2a5327c9",
+    "url": "/OtterPlayer/sw.js"
+  }
+]);
---
+++ precache-manifest.b7019d660f9967a07910e767ee4baab1.esm.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/OtterPlayer/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/OtterPlayer/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/OtterPlayer/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/OtterPlayer/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/OtterPlayer/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/OtterPlayer/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "c395157dc860738eef84bbf993a4168e",
+    "url": "/OtterPlayer/assets/icons/videojsFavicon.png"
+  },
+  {
+    "revision": "3cc4ab806f09c4fbcd67b59979913ff3",
+    "url": "/OtterPlayer/bundle.5ac15.esm.js"
+  },
+  {
+    "revision": "66be6940e205df3cc917",
+    "url": "/OtterPlayer/bundle.fd466.css"
+  },
+  {
+    "revision": "78a0c4e8e5ceff5afa7bb44ec1dc49be",
+    "url": "/OtterPlayer/index.html"
+  },
+  {
+    "revision": "f5cd293541a2e747939fcd169161bedc",
+    "url": "/OtterPlayer/polyfills.efc13.esm.js"
+  },
+  {
+    "revision": "57fec53d680749b4fe7f",
+    "url": "/OtterPlayer/route-about.chunk.39129.css"
+  },
+  {
+    "revision": "993410e802eedf3e442bbb4403ed64f8",
+    "url": "/OtterPlayer/route-about.chunk.eb728.esm.js"
+  },
+  {
+    "revision": "2bd36175f77eb1bcec23df93f42ce9f9",
+    "url": "/OtterPlayer/route-home.chunk.31c4c.esm.js"
+  },
+  {
+    "revision": "4b5d8f791a78ca07918c",
+    "url": "/OtterPlayer/route-home.chunk.bda64.css"
+  }
+]);
