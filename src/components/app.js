4476230730170fcedf73205228b11f03a5d74120
Jacky Hu <flameddd@gmail.com>
2020-01-04 00:29:26 +0800

    add ga

--- src/components/app.js
+++ src/components/app.js
@@ -66,6 +66,20 @@ export default class App extends Component {
 
   componentDidMount() {
     window.addEventListener("keyup", vidoeKeyUpListener);
+
+    const script = document.createElement("script");
+    const script2 = document.createElement('noscript');
+    
+    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WG3W59C');`;
+    script2.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WG3W59C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+
+    document.head.appendChild(script);
+    document.body.appendChild(script2);
   }
 
   componentWillUnmount() {
